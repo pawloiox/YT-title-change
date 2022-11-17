5eefbbff5c66669b5015274a975a2e078276125d
Paweł Giessler <pgiessler@neonet.pl>
2022-11-17 15:56:07 +0100

    First release, title has to be in code

---
+++ script.js
@@ -0,0 +1,7 @@
+(() => {
+  setTimeout(() => {
+    const title = document.querySelector("#title h1 yt-formatted-string");
+    title.innerHTML = "Jowita pokazuje ...";
+    title.classList.add("disp");
+  }, 5000);
+})();
